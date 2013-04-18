7300593d5395004594be9a7d9fd5186dc86ee019
Ernesto Tagwerker <ernesto@ombushop.com>
2013-04-18 18:35:53 +0200

    Rmoved database_connect method.

--- config/environment.rb
+++ config/environment.rb
@@ -7,12 +7,6 @@ require 'sequel'
 require 'compass'
 require 'sinatra/support/i18nsupport'
 
-# def database_connect(database_name, username, password, database_type = 'postgres')
-#   result = Sequel.connect(ENV["DATABASE_URL"] || 
-#     "#{database_type}://#{username}:#{password}@localhost/#{database_name}")
-#   result << "SET CLIENT_ENCODING TO 'UTF8';"
-# end
-
 Sinatra::Application.root = File.join(File.dirname(__FILE__),'..')
 
 module Faqtly
