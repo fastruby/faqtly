ec88891bcd39c2bae393c67ae112a536330a2746
Ernesto Tagwerker <ernesto@ombushop.com>
2013-04-23 15:33:13 +0200

    Pongo un puts para ver el path a Compass.

--- app.rb
+++ app.rb
@@ -7,6 +7,8 @@ require_relative 'helpers/init'
 require_relative 'models/init'
 require_relative 'routes/main'
 
+puts "Path to Compass: #{File.join(Faqtly::App.root, 'config', 'compass.rb').to_s}" 
+
 module Faqtly
   class App < Sinatra::Application
     # HTTP Authentication
