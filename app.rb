67e34bfbd5f32e723690211ca3c4434081c86703
Ernesto Tagwerker <ernesto@ombushop.com>
2014-06-10 09:17:32 -0300

    Added Faqtly::Base and refactored code from Application.

--- app.rb
+++ app.rb
@@ -10,35 +10,10 @@ require_relative 'helpers/init'
 require_relative 'models/init'
 require_relative 'routes/main'
 
-# puts "Path to Compass: #{File.join(Faqtly::App.root, 'config', 'compass.rb').to_s}" 
+# puts "Path to Compass: #{File.join(Faqtly::App.root, 'config', 'compass.rb').to_s}"
 
 module Faqtly
   class App < Sinatra::Application
-    # HTTP Authentication
-    set sessions: true
-
-    # HAML / SASS
-    set :haml, {format: :html5, escape_html: true}
-    set :scss, {style: :compact, debug_info: false}
-
-    # I18n
-    register Sinatra::I18nSupport
-
-    # We're going to load the paths to locale files
-    I18n.load_path += Dir[File.join(Sinatra::Application.root, 'config', 'locales', '*.yml').to_s]
-    load_locales Dir[File.join(Sinatra::Application.root, 'config', 'locales', '*.yml').to_s]
-    # default_places { File.join(Sinatra::Application.root, 'locales') }
-    
-    set :default_locale, 'es'
-    use Rack::MethodOverride
-    use Rack::Flash, sweep: true
-
-    helpers Rack::Utils
-    helpers Faqtly::Authentication
-    helpers Faqtly::RenderPartial
-    helpers Faqtly::SEO
-    helpers TagsHelper
-    helpers QuestionsHelper
 
     configure :production, :development, :test do
       enable :logging
@@ -47,7 +22,7 @@ module Faqtly
       set :views, File.join(App.root, 'views')
       set :public_folder, File.join(App.root, 'public')
       set :haml, { format: :html5 } # default Haml format is :xhtml
-      Compass.add_project_configuration(File.join(App.root, 'config', 'compass.rb'))  
+      Compass.add_project_configuration(File.join(App.root, 'config', 'compass.rb'))
       enable :clean_trace
     end
 
